4fdfbae9087d42148f56ca915265a45587378913
Josh Pollock <jpollock412@gmail.com>
2022-01-10 12:00:15 -0500

    try without chalk require

--- src/lib/log.js
+++ src/lib/log.js
@@ -1,5 +1,12 @@
-const chalk = require('chalk');
 const log = console.log;
+
+
+const chalk = {
+    green:(str) => str,
+    red:(str) => str,
+    yellow:(str) => str,
+    blue:(str) => str,
+}
 
 
 const maybeStringify = (obj) => {
@@ -9,7 +16,7 @@ const maybeStringify = (obj) => {
     return JSON.stringify(obj,null,2);
 }
 
-export const error = (message) => log(chalk.green(maybeStringify(message)));
+export const error = (message) => log((maybeStringify(message)));
 export const important = (message) => log(chalk.yellow(maybeStringify(message)));
 export const info = (message) => log(chalk.blue(maybeStringify(message)));
 export const success = (message) => log(chalk.green(maybeStringify(message)));
