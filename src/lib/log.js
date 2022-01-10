ca7a18b2ba419aa5f6a4d5afb0e895fa95c14885
Josh Pollock <jpollock412@gmail.com>
2022-01-10 12:10:55 -0500

    try require again

--- src/lib/log.js
+++ src/lib/log.js
@@ -1,13 +1,5 @@
+let chalk = require('chalk');
 const log = console.log;
-
-
-const chalk = {
-    green:(str) => str,
-    red:(str) => str,
-    yellow:(str) => str,
-    blue:(str) => str,
-}
-
 
 const maybeStringify = (obj) => {
     if( 'string' == typeof obj ) {
@@ -16,7 +8,7 @@ const maybeStringify = (obj) => {
     return JSON.stringify(obj,null,2);
 }
 
-export const error = (message) => log((maybeStringify(message)));
+export const error = (message) => log(chalk.green(maybeStringify(message)));
 export const important = (message) => log(chalk.yellow(maybeStringify(message)));
 export const info = (message) => log(chalk.blue(maybeStringify(message)));
 export const success = (message) => log(chalk.green(maybeStringify(message)));
