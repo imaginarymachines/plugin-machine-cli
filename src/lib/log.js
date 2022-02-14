b395a4e41cfbf681e8194840fc3efafba104cc7a
Josh Pollock <jpollock412@gmail.com>
2022-02-14 13:01:55 -0500

    use an export in log

--- src/lib/log.js
+++ src/lib/log.js
@@ -8,8 +8,16 @@ const maybeStringify = (obj) => {
     return JSON.stringify(obj,null,2);
 }
 
-export const error = (message) => log(chalk.green(maybeStringify(message)));
-export const important = (message) => log(chalk.yellow(maybeStringify(message)));
-export const warning = important;
-export const info = (message) => log(chalk.blue(maybeStringify(message)));
-export const success = (message) => log(chalk.green(maybeStringify(message)));
+const error = (message) => log(chalk.green(maybeStringify(message)));
+const important = (message) => log(chalk.yellow(maybeStringify(message)));
+const warning = important;
+const info = (message) => log(chalk.blue(maybeStringify(message)));
+const success = (message) => log(chalk.green(maybeStringify(message)));
+
+module.exports = {
+    error,
+    important,
+    warning,
+    info,
+    success,
+};
