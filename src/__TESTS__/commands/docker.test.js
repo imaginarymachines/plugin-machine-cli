5ba2653edf68a564e1b69fe2f9c47dab9cec7c54
Josh Pollock <jpollock412@gmail.com>
2022-04-01 17:11:02 -0400

    Run docker tests correctly

--- src/__TESTS__/commands/docker.test.js
+++ src/__TESTS__/commands/docker.test.js
@@ -2,20 +2,20 @@ describe( 'running commands to make sure there are no errors', () => {
     const shell = require('shelljs');
 
     it( 'Gets node version', () => {
-        shell.exec('node -v');
+        shell.exec('plugin-machine node -v');
     });
 
     it( 'Get npm version', () => {
-        shell.exec('npm -v');
+        shell.exec('plugin-machine npm -v');
     }
     );
 
     it( 'Gets yarn version', () => {
-        shell.exec('yarn -v');
+        shell.exec('plugin-machine yarn -v');
     }
     );
     it( 'Gets composer version', () => {
-        shell.exec('composer --version');
+        shell.exec('plugin-machine composer --version');
     }
     );
 });
