d7252c5683a212d7051076bed99254aed5f485fa
Josh Pollock <jpollock412@gmail.com>
2022-04-05 21:40:02 -0400

    Use correct variable

--- src/__TESTS__/commands/plugin.test.js
+++ src/__TESTS__/commands/plugin.test.js
@@ -6,15 +6,15 @@ describe( 'Calling plugin commands to check for errors', () => {
     });
 
     afterAll(() => {
-        shell.exec( 'unset PLUGIN_MACHINE_API_TOKEN' );
+        exec( 'unset PLUGIN_MACHINE_API_TOKEN' );
     });
 
     it( 'runs build command', () => {
-        shell.exec( 'plugin-machine plugin build' );
+        exec( 'plugin-machine plugin build' );
     });
 
     it( 'runs the zip commands', () => {
-        shell.exec( 'plugin-machine plugin zip' );
+        exec( 'plugin-machine plugin zip' );
     });
 
 
