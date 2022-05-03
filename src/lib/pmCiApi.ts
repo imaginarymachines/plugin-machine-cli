5baa4e9d0212044cf3faad1423970c4d1e7111a9
Josh Pollock <jpollock412@gmail.com>
2022-05-03 16:58:58 -0400

    Impliment new ci endpoints

--- src/lib/pmCiApi.ts
+++ src/lib/pmCiApi.ts
@@ -6,30 +6,37 @@ import {LocalFileData} from 'get-file-object-from-local-path';
 import Vapor from 'laravel-vapor'
 import axios from 'axios';
 const API = {
-    'root': `https://gentle-sands-hanbpakaz3zp.vapor-farm-f1.com`,
+    'root': `https://ci.pluginmachine.dev`,
     'sign': `/api/upload/sign`,
     'ack': `/api/upload/ack`
 }
 
-function pmCiApi(){
+function pmCiApi(token:string){
+    const headers ={
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+    }
 
     return {
         uploadVersion(
             file:File,
             pluginId: number
         ):Promise<LocalFileData>{
+            let options = {
+                content_type: file.type,
+                signedStorageUrl: `${API.root}${API.sign}`,
+                visibility: "private",
+                //@ts-ignore
+                progress: progress => {
+                    console.log({progress});
+                },
+                headers
+            };
             return new Promise( (resolve,reject) => {
                 //Upload file
-                Vapor.store(file, {
-                    content_type: file.type,
-                    signedStorageUrl: `${API.root}${API.sign}`,
-                    visibility: "private",
-                    //@ts-ignore
-                    progress: progress => {
-                        console.log({progress});
-                    }
-                })
+                Vapor.store(file, options).catch(e => console.log({e}))
                     .then((response:void|AxiosResponse) => {
+                        console.log(response.data);
                         axios.post(`${API.root}${API.ack}`, {
                             //@ts-ignore
                             uuid: response.uuid,
@@ -37,12 +44,10 @@ function pmCiApi(){
                             key: response.key,
                             name: file.name,
                             pluginId,
-                        }).then( (r:void|AxiosResponse) => {
+                        },{headers}).then( (r:void|AxiosResponse) => {
                             if(r && r.data.uuid){
-                                console.log(r.data);
                                 //@ts-ignore
-                                resolve({uuid:data.uuid})
-
+                                resolve({uuid:r.data.uuid})
                             }
                             reject(false);
                         })
